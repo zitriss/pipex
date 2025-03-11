5bb87bb2ae29a24b151080f9b0546b1c72c91909
Tristan Lize <tlize@c2r6p5.42lehavre.fr>
2025-03-11 14:45:56 +0100

    a new leaf

---
+++ includes/pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 16:49:24 by tlize             #+#    #+#             */
+/*   Updated: 2025/03/11 14:34:28 by tlize            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include "libft.h"
+
+#endif
