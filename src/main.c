feb9413d8b2479199c43fd25f2a43e18528ed115
Tristan Lize <tlize@c2r6p5.42lehavre.fr>
2025-03-11 19:17:43 +0100

    hmmmmmm

--- src/main.c
+++ src/main.c
@@ -6,11 +6,17 @@
 /*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 16:44:58 by tlize             #+#    #+#             */
-/*   Updated: 2025/03/11 14:34:31 by tlize            ###   ########.fr       */
+/*   Updated: 2025/03/11 17:58:31 by tlize            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+int check_ac(int ac)
+{
+    if (ac != 5)
+        error_msg("4 arguments sont necessaires.");
+}
 
 int main(int ac, char **av, char **envp)
 {
@@ -18,4 +24,7 @@ int main(int ac, char **av, char **envp)
     int pipe2[2];
     int i;
 
+    check_ac(ac);
+    pipe(pipe1);
+    pipe(pipe2);
 }
